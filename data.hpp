6cf51bedb53416be53eb4fb3895482850ebc83a3
paul-moreau <112432533+paul-moreau@users.noreply.github.com>
2022-10-28 13:41:20 +0200

    feat: class data

---
+++ data.hpp
@@ -0,0 +1,20 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+namespace data{
+    
+    class Data{
+        
+        private:
+            float[] _data;
+            int _nb_features;
+            int _nb_samples;
+
+        public:
+
+            Data();
+            ~Data();
+
+
+    }
+}
