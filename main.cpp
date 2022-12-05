001dd49fbda4374f12fb42945713bf3dc6c553a2
paul-moreau <112432533+paul-moreau@users.noreply.github.com>
2022-12-05 20:42:29 +0100

    feat: better management of arguments
    add of EnoughArguments() and TooMuchArguments()

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 static void help(void);
-static void readData(char *argv[]);
+static void readData(char *argv[],int argc);
 
 static struct option opts[] = {
 	{ "help", 0, 0, 'h' },
@@ -19,26 +19,27 @@ static struct option opts[] = {
 int main(int argc, char *argv[]){
     int c, option_index = 0;
     int p=0;
-    //Data test;
     c = getopt_long(argc, argv, "hr:g:c :", opts,
             &option_index);
     switch (c)
     {
         case 'h':
-            help();
+            cout << "you hit help" << endl;
+            cout << "argc = " << argc << endl;
+            //help();
             break;
 
         case 'r':
-            readData(argv);
-            //p=test.readFile(argv[2]);
+            //cout << "you hit readData" << endl;
+            readData(argv, argc);
             break;
 
         case 'g':
-            cout<<"g";
+            cout << "you hit guess" << endl;
             break;
 
         case 'c':
-            cout<<"c";
+            cout << "you hit compare" << endl;
             break;
     }
     return 0;
@@ -54,12 +55,29 @@ static void help(void)
 		"  -c --compare\t\t\tCompare functionning of <algorithm1> with <algorithm2> with <file> for <k> elements\n");
 }
 
-static void readData(char *argv[]){
+static void readData(char *argv[],int argc){
 
-    cout << "read Data started, pathfile in entry : " << endl;
-    cout << argv[2] << endl;
+    cout << "argc = " << argc << endl;
+    cout << "read Data started, pathfile in entry : " << argv[2] << endl;
 
-    Data* test = new Data();
+    bool training = 4;
+    if(argc>3){
+        cout << "training ? " << argv[3] << endl;
+        string train = argv[3];
+        for(auto & c : train){
+            c = toupper(c);
+        }
+        if(train=="TRUE"){
+            training = true;
+        }else{
+            training = false;
+        }
+    }
+    if(argc>4){
+        cout << "percentage of the file : " << argv[4] << endl;
+    }
+    cout << "bool training is = " << training << endl;
+    /*Data* test = new Data();
 
     test->print();
 
@@ -70,5 +88,6 @@ static void readData(char *argv[]){
     int nbOf2 = test->howMuch(2);
     cout << "There are " << nbOf2 << " data to describe the 2" << endl;
 
-    delete test;
+    delete test;*/
+    
 }
--- utils.cpp
+++ utils.cpp
@@ -14,3 +14,74 @@ vector<string> split (const string &s, char delim) {
 
     return result;
 }
+
+//Pas testée, pas sur que ça fonctionne
+void upper(string& s){
+    for(auto &c : s){
+            c = toupper(c);
+    }
+}
+
+//A compléter quand on sera plus précis sur ce qu'on veut pour les commandes excatement
+//Pas testée
+//Utile ? Pas sur que ça soit utile dans tous les cas (exemple de --help), mais dans le reste je pense que oui
+bool enoughArguments(int argc, int choice){
+    bool res = false;
+    switch(choice){
+        case 'h':
+            if(argc>1){
+                res = true;
+            }
+            break;
+        case 'r':
+            if(argc>2){
+                res = true;
+            }
+            break;
+
+        case 'g':
+            if(argc>1){
+                res = true;
+            }
+            break;
+
+        case 'c':
+            if(argc>1){
+                res = true;
+            }
+            break;
+    }
+    return res;
+}
+
+//A compléter quand on sera plus précis sur ce qu'on veut pour les commandes excatement
+//Pas testée
+//Utile ? Pas sur que ça soit utile dans tous les cas (exemple de --help), mais dans le reste je pense que oui
+bool TooMuchArguments(int argc, int choice){
+    bool res = true;
+    switch(choice){
+        case 'h':
+            if(argc<2){
+                res = false;
+            }
+            break;
+        case 'r':
+            if(argc<6){
+                res = false;
+            }
+            break;
+
+        case 'g':
+            if(argc<667){
+                res = false;
+            }
+            break;
+
+        case 'c':
+            if(argc<667){
+                res = false;
+            }
+            break;
+    }
+    return res;
+}
