31f5d726faac5fbd0b0a72f515ac743e94abdcb2
paul-moreau <112432533+paul-moreau@users.noreply.github.com>
2022-12-05 19:12:51 +0100

    feat: start of algorithm classes

---
+++ algorithm1.hpp
@@ -0,0 +1,19 @@
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+
+#include <iostream>
+#include "data.hpp"
+#include "algorithm.hpp"
+
+using namespace std;
+
+class Algorithm1{
+    private:
+
+    public:
+        Algorithm1();
+
+        ~Algorithm1();
+
+};
+#endif
